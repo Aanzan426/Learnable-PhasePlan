1693eb0964ab8859149874fcee789b6e1e3cfda6
Deepaansh Deepak Sial <zatchwinston@gmail.com>
2026-02-10 23:35:13 +0530

    Create header.php

---
+++ PHP/Task Manager/helpers/header.php
@@ -0,0 +1,20 @@
+<?php
+	include __DIR__.'/../src/config/db.php';
+?>
+
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title> Task Manager </title>
+		<link rel = "stylesheet" href = "tasks.css">
+		<script src = "tasks.js" defer></script>
+	</head>
+	<body>
+		<header>
+			<nav>
+				<a href = "#add tasks"> Add Tasks </a>
+				<a href = "#mark"> Mark Tasks </a>
+				<a href = "#delete tasks"> Delete Tasks </a>
+			</nav>
+		</header>
+		
